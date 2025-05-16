4f47adad0117f6cd29bbce6ee264ad8ebbb9a49e
Szeman Gergo <szeman.gergo@BOLYAI.LOCAL>
2025-05-16 08:54:23 +0200

    i do not like javascript

--- 1_promise_async/kitero.js
+++ 1_promise_async/kitero.js
@@ -7,3 +7,12 @@ function buyApple1(num){
 
 const almaPromise = buyApple1(4);
 
+console.log(almaPromise);
+almaPromise.then(value=>{
+    console.log(value);
+})
+console.log(almaPromise);
+
+buyApple1(6).catch(error => (console.log(error)));
+
+buyApple1(3).then(value=>{console.log(value)}).catch(error => (console.log(error)));
