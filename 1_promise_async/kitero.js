153d5b4c9931710996f30c13e3d6e65a8f58c855
Szeman Gergo <szeman.gergo@BOLYAI.LOCAL>
2025-05-16 09:33:17 +0200

    death2

--- 1_promise_async/kitero.js
+++ 1_promise_async/kitero.js
@@ -5,6 +5,24 @@ function buyApple1(num){
         return Promise.reject('Nincs ennyi almánk.');
 }
 
+function buyApple2(num){
+    return new Promise((resolve, reject)=>{
+        if (num < 5)
+            resolve('Van ' + num + ' alma.');
+        else
+            reject('Nincs ennyi almánk.');
+    })
+}
+
+function buyApple3(num){
+    return new Promise((resolve, reject)=>{
+        if (num < 5)
+            setTimeout(()=>{resolve('Van ' + num + ' alma.')},1000);
+        else
+            setTimeout(()=>{reject('Nincs ennyi almánk.')},1000);
+    })
+}
+
 const almaPromise = buyApple1(4);
 
 console.log(almaPromise);
@@ -15,4 +33,8 @@ console.log(almaPromise);
 
 buyApple1(6).catch(error => (console.log(error)));
 
-buyApple1(3).then(value=>{console.log(value)}).catch(error => (console.log(error)));
+buyApple1(3).then(value=>{console.log(value)}).catch(error=>(console.log(error)));
+
+buyApple2(2).then(value=>{console.log(value)}).catch(error=>(console.log(error)));
+
+buyApple3(1).then(value=>{console.log(value)}).catch(error=>(console.log(error)));
